fcab52e8c34b472443d0a4d4ae1fe1d3eea64bda
coutbiaafnts <bia.fontes653@gmail.com>
2023-10-12 16:29:51 -0300

    hash password

--- src/main/java/br/com/coutbiaafnts/todolist/user/UserController.java
+++ src/main/java/br/com/coutbiaafnts/todolist/user/UserController.java
@@ -8,17 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Modificadores:
- * public
- * private
- * protected
- */
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-   
+
    @Autowired
    private IUserRepository userRepository;
 
@@ -26,12 +21,16 @@ public class UserController {
    public ResponseEntity create(@RequestBody UserModel userModel) {
       var user = this.userRepository.findByUsername(userModel.getUsername());
 
-      if(user != null) {
+      if (user != null) {
          System.out.println("Usuário já existe!");
          // mensagem de erro
          // status code -> status dentro da requisição (200 sucesso || 400)
          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
       }
+
+      var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+      userModel.setPassword(passwordHashred);
 
       var userCreated = this.userRepository.save(userModel);
       return ResponseEntity.status(HttpStatus.OK).body(userCreated);
