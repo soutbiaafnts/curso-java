8aa9abf6019fe52b9e58ab31129c94abac813ff7
coutbiaafnts <bia.fontes653@gmail.com>
2023-10-12 00:30:06 -0300

    create table

--- src/main/java/br/com/coutbiaafnts/todolist/user/UserModel.java
+++ src/main/java/br/com/coutbiaafnts/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package br.com.coutbiaafnts.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data // coloca getter e setter para todos os atributos
+@Entity(name="tb_users") // nomeia a tabela
 public class UserModel {
+
+   @Id
+   @GeneratedValue(generator = "UUID") // jakarta cria o ID
+   private UUID id;
    
    private String username;
    private String name;
    private String password;
+
+   @CreationTimestamp
+   private LocalDateTime createdAt;
 
 }
