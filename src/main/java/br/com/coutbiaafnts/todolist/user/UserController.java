540d61c7602f334a6188fb308f9dd801c023373d
coutbiaafnts <bia.fontes653@gmail.com>
2023-10-12 00:57:06 -0300

    Response Entity

--- src/main/java/br/com/coutbiaafnts/todolist/user/UserController.java
+++ src/main/java/br/com/coutbiaafnts/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.coutbiaafnts.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,15 +23,17 @@ public class UserController {
    private IUserRepository userRepository;
 
    @PostMapping("/")
-   public UserModel create(@RequestBody UserModel userModel) {
+   public ResponseEntity create(@RequestBody UserModel userModel) {
       var user = this.userRepository.findByUsername(userModel.getUsername());
 
       if(user != null) {
          System.out.println("Usuário já existe!");
-         return null;
+         // mensagem de erro
+         // status code -> status dentro da requisição (200 sucesso || 400)
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
       }
 
       var userCreated = this.userRepository.save(userModel);
-      return userCreated;
+      return ResponseEntity.status(HttpStatus.OK).body(userCreated);
    }
 }
