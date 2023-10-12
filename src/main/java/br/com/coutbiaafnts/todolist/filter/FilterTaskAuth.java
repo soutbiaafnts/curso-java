db433a3e6839221a458358bf25b4e0d8d2562a0f
coutbiaafnts <bia.fontes653@gmail.com>
2023-10-12 18:39:57 -0300

    password decode | todo:separar rotas que verificam

--- src/main/java/br/com/coutbiaafnts/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/coutbiaafnts/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.coutbiaafnts.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.coutbiaafnts.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+   @Autowired
+   private IUserRepository userRepository;
 
    @Override
    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -35,18 +41,24 @@ public class FilterTaskAuth extends OncePerRequestFilter {
             String username = credentials[0];
             String password = credentials[1];
 
-            System.out.println("Authentication: ");
-            System.out.println(username);
-            System.out.println(password);
+            // valid user no BD
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+               response.sendError(401);
+            } else {
+               // valid password no BD
+               var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+               if (passwordVerify.verified){ // retorna (true || false)
+                  filterChain.doFilter(request, response);
+               } else {
+                  response.sendError(401);
+               }
+               // go
+   
+   
 
-            // valid user
+            }
 
-            // valid password
-
-            // go
-
-
-            filterChain.doFilter(request, response);
    }
    
 }
